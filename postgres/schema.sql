b80d3197a5a1d8964390e59fee9e561918ada62f
ABCurado <1140280@isep.ipp.pt>
2017-11-18 20:50:29 +0000

    Add database configurations

---
+++ postgres/schema.sql
@@ -0,0 +1,50 @@
+-- Comment
+
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL DEFAULT '',
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+
+CREATE TABLE meals (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL DEFAULT '',
+    type TEXT NOT NULL,
+    people INTEGER,
+    day DATE NOT NULL ,
+    price_per_person NUMERIC(2),
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE shops (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL DEFAULT '',
+    day DATE NOT NULL ,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE stocks (
+    id INTEGER PRIMARY KEY,
+    product_id INTEGER REFERENCES products (id) ,
+    amount INTEGER ,
+    price NUMERIC(2),
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE shop_products (
+    product_id INTEGER REFERENCES products (id),
+    sale_id INTEGER REFERENCES shops (id),
+    amount  NUMERIC(2),
+    price  NUMERIC(2)
+);
+
+CREATE TABLE meal_products (
+    product_id INTEGER REFERENCES products (id),
+    meal_id INTEGER REFERENCES meals (id),
+    amount  NUMERIC(2),
+    price  NUMERIC(2)
+);
