c484e4d653750ce9a79ec317bbf1536844112f31
ABCurado <1140280@isep.ipp.pt>
2017-11-21 19:39:35 +0000

    add body parser so express can parse post requests

--- api/src/app.js
+++ api/src/app.js
@@ -7,6 +7,12 @@ var express = require('express')
 var app = express()
 
 var routes = require('./routes/routes')
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.use('/', routes)
 /*
